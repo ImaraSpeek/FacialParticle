e9edcee4db10134d99a3cea3ddf9b2187b46570e
basvank <basvk@hetnet.nl>
2014-05-14 11:17:12 +0200

    Added lock states

--- WatchDog/src/com/watchdog/ApplicationState.java
+++ WatchDog/src/com/watchdog/ApplicationState.java
@@ -18,4 +18,27 @@ public class ApplicationState {
 		return instance;
 	}
 	
+	
+	/**
+	 * LOCK STATE
+	 */
+	// Unlocked state, default
+	public final int LOCK_STATE_UNLOCKED = 0;
+	// Locking state, started when user clicks 'lock' and is in the process of placing the phone on a surface
+	public final int LOCK_STATE_LOCKING = 1;
+	// Calibrating state, started when phone is placed on surface and the accelerometer is being calibrated
+	public final int LOCK_STATE_CALIBRATING = 2;
+	// Locked mode, started when the phone is calibrated and is in lock mode
+	public final int LOCK_STATE_LOCKED = 3;
+	
+	private int state = LOCK_STATE_UNLOCKED;
+	
+	public int getState() {
+		return state;
+	}
+	
+	public void setState(int s) {
+		state = s;
+	}
+	
 }
