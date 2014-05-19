3ec51fcfb42b6e6fc9b3c0412a839d0c7166bc9f
Imara90 <imaraspeek@gmail.com>
2014-05-19 12:29:07 +0200

    new folder for OpenCV

---
+++ DirectCamera/jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call DIRCAM)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := 
+LOCAL_SRC_FILES :=
+
+ 
+<some variable name> := <some variable value>
+<some variable name> := <some variable value>
+
+include $(BUILD_SHARED_LIBRARY)
