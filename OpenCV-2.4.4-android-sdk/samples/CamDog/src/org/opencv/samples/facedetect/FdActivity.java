eb58bd267217364c54a42e46354ca693afe35fb8
Imara90 <imaraspeek@gmail.com>
2014-06-10 21:36:50 +0200

    used template for eyes also sort of on the mouth to detect darkest part, have to open mouth slightly for it to work, this calibration should however not be necessary anymore after 1 usage

--- OpenCV-2.4.4-android-sdk/samples/CamDog/src/org/opencv/samples/facedetect/FdActivity.java
+++ OpenCV-2.4.4-android-sdk/samples/CamDog/src/org/opencv/samples/facedetect/FdActivity.java
@@ -75,6 +75,7 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
     private Mat                    mResult;
     private Mat					   templateR;
     private Mat					   templateL;
+    private Mat					   templateM;
     
     private File                   mCascadeFile;
     
@@ -86,6 +87,7 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
     private CascadeClassifier      mFaceDetector;
     private CascadeClassifier	   mCascadeER;
     private CascadeClassifier	   mCascadeEL;
+    private CascadeClassifier	   mCascadeM;
     
     private int                    mDetectorType       = NATIVE_DETECTOR;
     private String[]               mDetectorName;
@@ -109,10 +111,13 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
     private static final int TM_CCORR_NORMED 	= 5;
     
     public static int		 method				= 1;
+    
+    Point left_pupil = null;
+	Point right_pupil = null;
 
     private long				    starttime = 0;
     private int						learn_frames = 0;
-    private double					match_value;
+    private double					match_valuel, match_valuer;
     
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -156,9 +161,9 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
                     // Load left eye classifier
                     try {
                         // load cascade file from application resources
-                        InputStream isEL = getResources().openRawResource(R.raw.haarcascade_eye);
+                        InputStream isEL = getResources().openRawResource(R.raw.haarcascade_lefteye_2splits);
                         File cascadeDirEL = getDir("cascade", Context.MODE_PRIVATE);
-                        File mCascadeFileEL = new File(cascadeDirEL, "haarcascade_eye.xml");
+                        File mCascadeFileEL = new File(cascadeDirEL, "haarcascade_lefteye_2splits.xml");
                         FileOutputStream osEL = new FileOutputStream(mCascadeFileEL);
 
                         byte[] bufferEL = new byte[4096];
@@ -189,9 +194,9 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
                     // Load right eye classifier
                     try {
                         // load cascade file from application resources
-                        InputStream isER = getResources().openRawResource(R.raw.haarcascade_eye);
+                        InputStream isER = getResources().openRawResource(R.raw.haarcascade_righteye_2splits);
                         File cascadeDirER = getDir("cascade", Context.MODE_PRIVATE);
-                        File mCascadeFileER = new File(cascadeDirER, "haarcascade_eye.xml");
+                        File mCascadeFileER = new File(cascadeDirER, "haarcascade_righteye_2splits.xml");
                         FileOutputStream osER = new FileOutputStream(mCascadeFileER);
 
                         byte[] bufferER = new byte[4096];
@@ -219,41 +224,40 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
                         Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
                     }
                     
-                    /*                    
+                    // Load the mouth classifier                   
                     try {
                         // load cascade file from application resources
-                        InputStream ismouth = getResources().openRawResource(R.raw.haarcascade_mcs_mouth);
-                        File cascadeDirmouth = getDir("cascade", Context.MODE_PRIVATE);
-                        mCascadeFilemouth = new File(cascadeDirmouth, "haarcascade_mcs_mouth.xml");
-                        FileOutputStream osmouth = new FileOutputStream(mCascadeFilemouth);
+                        InputStream isM = getResources().openRawResource(R.raw.haarcascade_mcs_mouth);
+                        File cascadeDirM = getDir("cascade", Context.MODE_PRIVATE);
+                        File mCascadeFileM = new File(cascadeDirM, "haarcascade_mcs_mouth.xml");
+                        FileOutputStream osM = new FileOutputStream(mCascadeFileM);
 
-                        byte[] buffermouth = new byte[4096];
-                        int bytesReadmouth;
-                        while ((bytesReadmouth = ismouth.read(buffermouth)) != -1) {
-                            osmouth.write(buffermouth, 0, bytesReadmouth);
+                        byte[] bufferM = new byte[4096];
+                        int bytesReadM;
+                        while ((bytesReadM = isM.read(bufferM)) != -1) {
+                            osM.write(bufferM, 0, bytesReadM);
                         }
-                        ismouth.close();
-                        osmouth.close();
+                        isM.close();
+                        osM.close();
                         
                         // This part is for the java cascade classifier to search within region
-                        mMouthDetector = new CascadeClassifier(mCascadeFilemouth.getAbsolutePath());
-                        if (mMouthDetector.empty()) {
-                            Log.e(TAG, "Failed to load mouth cascade classifier");
-                            mMouthDetector = null;
+                        mCascadeM = new CascadeClassifier(mCascadeFileM.getAbsolutePath());
+                        if (mCascadeM.empty()) {
+                            Log.e(TAG, "Failed to load eye cascade classifier");
+                            mCascadeM = null;
                         } else
                         {
-                            Log.i(TAG, "Loaded eye cascade classifier from " + mCascadeFilemouth.getAbsolutePath());
+                            Log.i(TAG, "Loaded M cascade classifier from " + mCascadeFileM.getAbsolutePath());
                         }
-
-                        // create detector mouth
-                        //mNativeDetectormouth = new DetectionBasedTracker(mCascadeFilemouth.getAbsolutePath(), 0);
-
-                        cascadeDirmouth.delete();
+                        
+                        cascadeDirM.delete();
 
                     } catch (IOException e) {
                         e.printStackTrace();
                         Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
                     }
+                    
+                    /*
                     try {
                         // load cascade file from application resources
                         InputStream isnose = getResources().openRawResource(R.raw.haarcascade_mcs_nose);
@@ -401,16 +405,27 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
 	        Core.rectangle(mRgba, moutharea.tl(), moutharea.br(), MOUTH_RECT_COLOR, 2);
             
 	        // learn the template for the eyes
-	        if(learn_frames<5)
+	        if(learn_frames<10)
 	        {
 	        	templateL = get_template(mCascadeEL,eyearea_left,24);
              	templateR = get_template(mCascadeER,eyearea_right,24);
+             	// have to open mouth slightly for it to calibrate
+             	templateM = get_templateMouth(mCascadeM, moutharea,24);
+             	
              	learn_frames++;
             }
 	        else
 	        {
-	        	match_value = match_eye(eyearea_left,templateL,FdActivity.method); 
-	        	match_value = match_eye(eyearea_right,templateR,FdActivity.method); 
+	        	// match_value is the cenrtainty that it is the pupil
+	        	match_valuel = match_eye(eyearea_left,templateL, left_pupil, FdActivity.method); 
+	        	match_valuer = match_eye(eyearea_right,templateR, right_pupil, FdActivity.method); 
+	        	
+	        	// TODO find out why match_valuer stays 1.0
+	        	// Log.i("distance", "match left: " + match_valuel + "matchvalue right: " + match_valuer);
+	        	
+	        	// TODO find the distance between the 2 points
+	        	
+	        	
 	        }
         }
         
@@ -463,89 +478,6 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
 	                trackface = cs.camshift_track_face(mRgba, facesArray, cs);            
 	                // Convert the rotated rectangle from cam shifting to a regular rectangle 
 	                trackhue = trackface.boundingRect();
-	            }
-	        }   
-	       
-		    if (facedetected)
-		    	
-		    {
-		    	//if(trackhue.x > 0 && trackhue.y > 0)
-		    	if (trackhue.width > 0 && trackhue.height > 0)
-	        	{
-		    	
-		            // compute the eye area
-		           	//Rect eyearea = new Rect(trackhue.x +trackhue.width/8,(int)(trackhue.y + (trackhue.height/4.5)),trackhue.width - trackhue.width/8,(int)( trackhue.height/2.0));
-		            // split it
-		            Rect eyearea_right = new Rect(trackhue.x +trackhue.width/16,(int)(trackhue.y + (trackhue.height/4.5)),(trackhue.width - trackhue.width/16)/2,(int)( trackhue.height/4));
-		            Rect eyearea_left = new Rect(trackhue.x +trackhue.width/16 + (trackhue.width - 2*trackhue.width/16)/2,(int)(trackhue.y + (trackhue.height/4.5)),(trackhue.width - trackhue.width/16)/2,(int)( trackhue.height/4));
-		            // draw the area - mGray is working grayscale mat, if you want to see area in rgb preview, change mGray to mRgba
-		            Core.rectangle(mRgba, eyearea_left.tl(),eyearea_left.br() , DEBUG_RECT_COLOR, 2);
-		            Core.rectangle(mRgba, eyearea_right.tl(),eyearea_right.br(), DEBUG_RECT_COLOR, 2);
-		            
-		            
-		            mEyeGrayLeft = mGray.adjustROI((eyearea_left.y - eyearea_left.height), eyearea_left.y, eyearea_left.x, (eyearea_left.x + eyearea_left.width));
-		            mEyeDetector.detectMultiScale(mEyeGrayLeft, lefteye, 1.1,2,2,new Size(), new Size());
-		            eyeleftArray = lefteye.toArray();
-		            for (int j = 0; j < eyeleftArray.length; j++){
-		            	Core.rectangle(mEyeRgbaLeft, eyeleftArray[0].tl(), eyeleftArray[0].br(), EYES_RECT_COLOR, 3);
-		            }
-		            
-		            
-		            mEyeGrayLeft = mGray.submat(eyearea_left);
-		            mEyeRgbaLeft = mRgba.submat(eyearea_left);
-		            mEyeGrayRight = mGray.submat(eyearea_right);
-		            mEyeRgbaRight = mRgba.submat(eyearea_right);
-		            
-		            // Java detector performs betters, detect eyes
-		            mEyeDetector.detectMultiScale(mEyeGrayLeft, lefteye, 1.1,2,2,new Size(), new Size());
-		            mEyeDetector.detectMultiScale(mEyeGrayRight, righteye, 1.1,2,2,new Size(), new Size());
-		            
-		            // transform to arrays and print the eyes
-		            eyeleftArray = lefteye.toArray();
-		            eyerightArray = righteye.toArray();
-		            for (int j = 0; j < eyeleftArray.length; j++){
-		            	Core.rectangle(mEyeRgbaLeft, eyeleftArray[0].tl(), eyeleftArray[0].br(), EYES_RECT_COLOR, 3);
-		            }
-		            for (int j = 0; j < eyerightArray.length; j++){
-		            	Core.rectangle(mEyeRgbaRight, eyerightArray[0].tl(), eyerightArray[0].br(), EYES_RECT_COLOR, 3);
-		            }	            
-		            
-		            
-		            // compute the mouth area
-			        Rect moutharea = new Rect(trackhue.x +trackhue.width/8,(int)(trackhue.y + trackhue.height/2),trackhue.width - trackhue.width/8,(int)( trackhue.height/3.0));
-			        Core.rectangle(mRgba, moutharea.tl(), moutharea.br(), DEBUG_RECT_COLOR, 2);
-		            
-			        mMouthGray = mGray.submat(moutharea);
-		            mMouthRgba = mRgba.submat(moutharea);
-		            
-		            mMouthDetector.detectMultiScale(mMouthGray, mouths, 1.1, 3, 2, new Size(), new Size());
-		        	//mNativeDetectormouth.detect(mGray, mouths);
-		        	mouthsArray = mouths.toArray();
-		            //for (int j = 0; j < mouthsArray.length; j++)
-		            //{
-		            //	Core.rectangle(mMouthRgba, mouthsArray[j].tl(), mouthsArray[j].br(), MOUTH_RECT_COLOR, 3);            	
-		            //}
-		        	if (mouthsArray.length > 0)
-		        	{
-		        		Core.rectangle(mMouthRgba, mouthsArray[0].tl(), mouthsArray[0].br(), MOUTH_RECT_COLOR, 3);            	
-		        	}
-		           
-			        
-			        /*
-			        // compute the nose area
-			        Rect nosearea = new Rect(trackhue.x +trackhue.width/8,(int)(trackhue.y + trackhue.height/2),trackhue.width - trackhue.width/4,(int)( trackhue.height/3.5));
-			        Core.rectangle(mRgba, moutharea.tl(), moutharea.br(), DEBUG_RECT_COLOR, 2);
-		            
-			        mNoseGray = mGray.submat(nosearea);
-			        mNoseRgba = mRgba.submat(nosearea);
-			        mNoseDetector.detectMultiScale(mNoseGray, noses, 1.1,2,2,new Size(), new Size());
-		            
-		        	//mNativeDetectornose.detect(mGray, noses);
-		        	nosesArray = noses.toArray();
-		            for (int j = 0; j < nosesArray.length; j++)
-		            {
-		            	Core.rectangle(mNoseRgba, nosesArray[j].tl(), nosesArray[j].br(), NOSE_RECT_COLOR, 3);            	
-		            }
 		            
 	        	}	      
 		    }
@@ -553,7 +485,7 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
         return mRgba;
     }
     
-    private double  match_eye(Rect area, Mat mTemplate,int type){
+    private double  match_eye(Rect area, Mat mTemplate, Point pupil_coord, int type){
 		  Point matchLoc; 
 		  Mat mROI = mGray.submat(area);
 	      int result_cols =  mGray.cols() - mTemplate.cols() + 1;
@@ -593,6 +525,9 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
 		  
 		  Point  matchLoc_tx = new Point(matchLoc.x+area.x,matchLoc.y+area.y);
 		  Point  matchLoc_ty = new Point(matchLoc.x + mTemplate.cols() + area.x , matchLoc.y + mTemplate.rows()+area.y );
+		  
+		  // Save the coordinates in the pupil coordinate reserved
+		  pupil_coord = new Point(matchLoc.x, matchLoc.y);
 		 
 		  Core.rectangle(mRgba, matchLoc_tx,matchLoc_ty, new Scalar(255, 255, 0, 255));
 		 
@@ -614,25 +549,58 @@ public class FdActivity extends Activity implements CvCameraViewListener2 {
     	 
     	Rect[] eyesArray = eyes.toArray();
     	for (int i = 0; i < eyesArray.length; i++){
-    	Rect e = eyesArray[i];
-    	e.x = area.x + e.x;
-    	e.y = area.y + e.y;
-    	Rect eye_only_rectangle = new Rect((int)e.tl().x,(int)( e.tl().y + e.height*0.4),(int)e.width,(int)(e.height*0.6));
-    	// reduce ROI
-    	mROI = mGray.submat(eye_only_rectangle);
-    	Mat vyrez = mRgba.submat(eye_only_rectangle);
-    	// find the darkness point
-    	Core.MinMaxLocResult mmG = Core.minMaxLoc(mROI);
-    	// draw point to visualise pupil
-    	Core.circle(vyrez, mmG.minLoc,2, new Scalar(255, 255, 255, 255),2);
-    	iris.x = mmG.minLoc.x + eye_only_rectangle.x;
-    	iris.y = mmG.minLoc.y + eye_only_rectangle.y;
-    	eye_template = new Rect((int)iris.x-size/2,(int)iris.y-size/2 ,size,size);
-    	Core.rectangle(mRgba,eye_template.tl(),eye_template.br(),new Scalar(255, 0, 0, 255), 2);
-    	// copy area to template
-    	template = (mGray.submat(eye_template)).clone();
+	    	Rect e = eyesArray[i];
+	    	e.x = area.x + e.x;
+	    	e.y = area.y + e.y;
+	    	Rect eye_only_rectangle = new Rect((int)e.tl().x,(int)( e.tl().y + e.height*0.4),(int)e.width,(int)(e.height*0.6));
+	    	// reduce ROI
+	    	mROI = mGray.submat(eye_only_rectangle);
+	    	Mat vyrez = mRgba.submat(eye_only_rectangle);
+	    	// find the darkness point
+	    	Core.MinMaxLocResult mmG = Core.minMaxLoc(mROI);
+	    	// draw point to visualise pupil
+	    	Core.circle(vyrez, mmG.minLoc,2, new Scalar(255, 255, 255, 255),2);
+	    	iris.x = mmG.minLoc.x + eye_only_rectangle.x;
+	    	iris.y = mmG.minLoc.y + eye_only_rectangle.y;
+	    	eye_template = new Rect((int)iris.x-size/2,(int)iris.y-size/2 ,size,size);
+	    	Core.rectangle(mRgba,eye_template.tl(),eye_template.br(),new Scalar(255, 0, 0, 255), 2);
+	    	// copy area to template
+	    	template = (mGray.submat(eye_template)).clone();
+	    	return template;
+	    	}
     	return template;
     	}
+    
+    // get template for the pupils
+    private Mat  get_templateMouth(CascadeClassifier clasificator, Rect area,int size){
+    	Mat template = new Mat();
+    	Mat mROI = mGray.submat(area);
+    	MatOfRect mouths = new MatOfRect();
+    	Point lips = new Point();
+    	Rect mouth_template = new Rect();
+    	clasificator.detectMultiScale(mROI, mouths, 1.15, 2,Objdetect.CASCADE_FIND_BIGGEST_OBJECT|Objdetect.CASCADE_SCALE_IMAGE, new Size(30,30),new Size());
+    	 
+    	Rect[] mouthArray = mouths.toArray();
+    	for (int i = 0; i < mouthArray.length; i++){
+	    	Rect e = mouthArray[i];
+	    	e.x = area.x + e.x;
+	    	e.y = area.y + e.y;
+	    	Rect mouth_only_rectangle = new Rect((int)e.tl().x,(int)( e.tl().y + e.height*0.4),(int)e.width,(int)(e.height*0.6));
+	    	// reduce ROI
+	    	mROI = mGray.submat(mouth_only_rectangle);
+	    	Mat vyrez = mRgba.submat(mouth_only_rectangle);
+	    	// find the darkness point
+	    	Core.MinMaxLocResult mmG = Core.minMaxLoc(mROI);
+	    	// draw point to visualise pupil
+	    	Core.circle(vyrez, mmG.minLoc,2, new Scalar(255, 255, 255, 255),2);
+	    	lips.x = mmG.minLoc.x + mouth_only_rectangle.x;
+	    	lips.y = mmG.minLoc.y + mouth_only_rectangle.y;
+	    	mouth_template = new Rect((int)lips.x-size/2,(int)lips.y-size/2 ,size,size);
+	    	Core.rectangle(mRgba,mouth_template.tl(),mouth_template.br(),new Scalar(255, 0, 0, 255), 2);
+	    	// copy area to template
+	    	template = (mGray.submat(mouth_template)).clone();
+	    	return template;
+	    	}
     	return template;
     	}
 
